4e42ab14567b2b25f9eaedbf58ac72cef02d5850
Nikhil Gumasthi <teslappop9090@gmail.com>
2024-05-16 18:33:38 +0530

    added BFS

--- Graphs/bfs/shortestPathInBinMAtrix.cpp
+++ Graphs/bfs/shortestPathInBinMAtrix.cpp
@@ -1,48 +1,57 @@
 // 1091. Shortest Path in Binary Matrix
 // https://leetcode.com/problems/shortest-path-in-binary-matrix/
 
-
 class Solution {
 public:
-
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        //setting variables
-        int n = grid.size();
-        int m = grid[0].size();
-        int level = 0;
-        int dx[]={0,0,1,1,1,-1,-1,-1};
-        int dy[]={1,-1,0,1,-1,0,1,-1};
-
-        // false condirion
-        if(grid[0][0]){
+        
+        if(grid[0][0] == 1) {
             return -1;
         }
 
-        queue<pair<int,int>> que;
-        que.push({0,0});
+        int length = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<bool>> visited(n, vector<bool>(m,0));
 
-        while(!que.empty()){
-            int len = que.size();
-            level++;        
-            while(len--){
-                int x = que.front().first;
-                int y = que.front().second;
+        q.push(make_pair(0,0));
+        visited[0][0] = true;
 
-                if(x == n-1 && y == m-1){
-                    return level;
+        while(!q.empty()) {
+            
+            int size = q.size();
+            length++;
+
+            while(size--) {
+
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+
+                if(x==n-1 && y==m-1) {
+                    return length;
                 }
-                que.pop();
-                for(int i = 0;i<8;i++){
-                    int nextx = x + dx[i];
-                    int nexty = y + dy[i];
 
-                    if(nextx >= 0 && nextx < n && nexty >=0 && nexty < m && grid[nextx][nexty] == 0){
-                        grid[nextx][nexty] = 1; // this prevents us from going back to the same direction important
-                        que.push({nextx,nexty});
+                // diagonal directions
+                vector<pair<int,int>> directions = {{1,1},{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{-1,1},{1,-1}};
+
+                for(auto dir:directions) {
+
+                    int dx = x + dir.first;
+                    int dy = y + dir.second;
+
+                    if(dx>=0 && dx<n && dy>=0 && dy<m && grid[dx][dy]==0 && !visited[dx][dy]) {
+                        q.push(make_pair(dx,dy));
+                        visited[dx][dy] = true;
                     }
                 }
+
             }
+
         }
+
         return -1;
+
     }
 };
